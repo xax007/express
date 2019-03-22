59c2c683cc2e3cc9a1c99a29551715a44d7afca3
xax007 <44893059+xax007@users.noreply.github.com>
2019-03-22 23:27:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 
 app.post('*', (req, res) => {
-    res.write('<h1><marquee direction=right>Hello from Express path `/` on Now 2.0!</marquee></h1>')
-    res.write('<h2>Go to <a href="/about">/about</a></h2>')
     res.write(req.body.username)
     res.end()
 })
